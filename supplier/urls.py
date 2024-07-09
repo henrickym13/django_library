5c76b9f713787ccc0349b2304b9daf02ef3137da
unknown <henrique.so.ad@hotmail.com>
2024-07-09 15:39:16 -0300

    add app supplier

---
+++ supplier/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('supplier/', views.SupplierListView.as_view(), name='supplier_list'),
+    path('supplier/create/', views.SupplierCreateView.as_view(), name='supplier_create'),
+    path('supplier/<int:pk>/detail/', views.SupplierDetailView.as_view(), name='supplier_detail'),
+    path('supplier/<int:pk>/update/', views.SupplierUpdateView.as_view(), name='supplier_update'),
+    path('supplier/<int:pk>/delete/', views.SupplierDeleteView.as_view(), name='supplier_delete')
+]
